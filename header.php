fd5144947fbec1357301516f1a617411cf122386
BuildTools <unconfigured@null.spigotmc.org>
2018-03-18 15:02:23 -0300

    Login e logout funcionando.

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pt">
   <head>
@@ -12,12 +16,20 @@
              <li><a href="index.php">Home</a></li>
            </ul>
            <div class="nav-login">
-             <form>
-               <input type="text" name="username" placeholder="Username/E-mail">
-               <input type="password" name="password" placeholder="password">
-               <button type="submit" name="submit">Login</button>
-             </form>
-             <a href="signup.php">Sign Up</a>
+             <?php
+              if (isset($_SESSION['u_id'])) {
+                echo '<form action="includes/logout.inc.php" method="POST">
+                          <button type="submit" name="submit">Logout</button>
+                      </form>';
+              } else {
+                  echo '<form action="includes/login.inc.php" method="POST">
+                          <input type="text" name="username" placeholder="Username/E-mail">
+                          <input type="password" name="password" placeholder="password">
+                          <button type="submit" name="submit">Login</button>
+                        </form>
+                        <a href="signup.php">Sign Up</a>';
+              }
+             ?>
            </div>
          </div>
       </nav>
--- index.php
+++ index.php
@@ -5,6 +5,13 @@
     <section class="main-container">
       <div class="main-wrapper">
         <h2>Home</h2>
+        <?php
+            if (isset($_SESSION['u_id'])) {
+                echo "<br> Hello ";
+                echo $_SESSION['u_username'];
+                echo "<br> You are logged in!";
+            }
+        ?>
       </div>
     </section>
 
